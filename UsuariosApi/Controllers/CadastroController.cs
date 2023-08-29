97eca6adb6e2f1b696436d2f6837c6067666d990
Rafael Menezes <rafa.rcm@hotmail.com>
2023-08-28 21:56:15 -0300

    Iniciando projeto UsuarioApi

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastroUsuario(CreateUsuarioDto createDto)
+        {
+            return Ok();
+        }
+    }
+}
