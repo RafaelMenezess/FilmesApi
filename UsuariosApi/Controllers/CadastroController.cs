2ec7cb4a213a9e2431bfa515f39ab7f2f4c27b4a
Rafael Menezes <rafa.rcm@hotmail.com>
2023-08-29 22:04:02 -0300

    Criando Identity

--- UsuariosApi/Controllers/CadastroController.cs
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using UsuariosApi.Data.Dtos;
 using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
 
