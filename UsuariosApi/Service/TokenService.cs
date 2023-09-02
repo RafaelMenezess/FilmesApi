d7748ca08026afd8f1ccfdb667b2f9c063d37256
Rafael Menezes <rafa.rcm@hotmail.com>
2023-09-02 11:23:11 -0300

    Gerando Token

---
+++ UsuariosApi/Service/TokenService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Service
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> usuario)
+        {
+            Claim[] direitosUsuario = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("ergqervwefrgrthtyjryuyh3wwerqfqewfd"));
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: direitosUsuario,
+                signingCredentials: credenciais,
+                expires: DateTime.UtcNow.AddHours(1));
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return new Token(tokenString);
+        }
+    }
+}
